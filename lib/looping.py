eadf2fbd304d5451d83d5297a5e0d0db7ca33c26
dancan <dancan.morara@student.moringaschool.com>
2025-05-11 18:12:33 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,32 @@
 
 def happy_new_year():
     # code goes here!
+    numbers = 10
+    while numbers > 0:
+        print(numbers)
+        numbers -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    squares = [num * num for num in int_list]
+    print(squares)
+    return squares
+
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
+
+fizzbuzz()
